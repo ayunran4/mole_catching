413dffde2328732190953df0ec2c381db644dbe7
Rubin <ayunran4@gmail.com>
2017-07-27 13:56:58 +0900

    Update SW Code

--- CODE/SW/sw.c
+++ CODE/SW/sw.c
@@ -1,6 +1,6 @@
 /*
 ********************************************************************************
-* File        	: LED.C
+* File        	: SW.C
 * By          	: CJY
 *
 ********************************************************************************
@@ -9,22 +9,43 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define DDR_LED		DDRB
+#define DDR_SW		DDRD
+#define PIN_SW		PIND
+
+#define DDR_LED 	DDRB
 #define PORT_LED	PORTB
 
 int main(void)
-{
-    int i, j;
-   
+{  
+    //DDR_SW |= (0<<PIN0)|(0<<PIN1)|(0<<PIN2)|(0<<PIN3);
+	DDRD = 0x00;
     DDR_LED |= (1<<PORT4)|(1<<PORT5)|(1<<PORT6)|(1<<PORT7);
-    PORT_LED = 0xFF;
+    
+	PORT_LED = 0xFF;
 
     for (;;)
     {
-        PORT_LED = 0x00;
-    	_delay_ms(200);		
-        PORT_LED = 0xFF;
-    	_delay_ms(200);		
+		if (PIND == 0x01)
+		{
+			PORT_LED = 0xFE;
+			_delay_ms(200);
+			PORT_LED = 0xFF;
+		}
+		/*if (!(PIN_SW & 0x02))
+		{
+			PORT_LED = (0<<PORT1);
+			_delay_ms(200);
+		}
+		if (!(PIN_SW & 0x04))
+		{
+			PORT_LED = (0<<PORT2);
+			_delay_ms(200);
+		}
+		if (!(PIN_SW & 0x08))
+		{
+			PORT_LED = (0<<PORT3);
+			_delay_ms(200);
+		}*/
     }
 }
 
