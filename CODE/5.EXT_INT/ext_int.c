068c4835ec2a9e851c1e894e8bf5f785634e68c3
Rubin <ayunran4@gmail.com>
2017-07-31 19:27:22 +0900

    Update external interrupt code

--- CODE/5.EXT_INT/ext_int.c
+++ CODE/5.EXT_INT/ext_int.c
@@ -27,7 +27,27 @@ ISR(INT0_vect)
   if (cnt > 9)
     cnt = 0;
 }
-
+// External Interrupt1
+ISR(INT1_vect)
+{
+  cnt++;
+  if (cnt > 9)
+    cnt = 0;
+}
+// External Interrupt2
+ISR(INT2_vect)
+{
+  cnt++;
+  if (cnt > 9)
+    cnt = 0;
+}
+// External Interrupt3
+ISR(INT3_vect)
+{
+  cnt++;
+  if (cnt > 9)
+    cnt = 0;
+}
 int main(void)
 {
   unsigned char num[10] = {0x0F,0x1F,0x2F,0x3F,0x4F,0x5F,0x6F,0x7F,0x8F,0x9F};
@@ -36,20 +56,23 @@ int main(void)
   DDR_FND |= (1<<PORT4)|(1<<PORT5)|(1<<PORT6)|(1<<PORT7);
   DDR_LED |= (1<<PORT4)|(1<<PORT5)|(1<<PORT6)|(1<<PORT7);
 
-  EICRA = (1<<ISC01)|(0<<ISC00);  // Rising edge
-  EIMSK = (1<<INT0);
+  EICRA = (1<<ISC01)|(0<<ISC00)|(1<<ISC11)|(0<<ISC10)|(1<<ISC21)|(0<<ISC20)|(1<<ISC31)|(0<<ISC30);  // Falling edge
+  EIMSK = (1<<INT0)|(1<<INT1)|(1<<INT2)|(1<<INT3);
+
+  UCSR1B = (0<<RXEN1)|(0<<TXEN1);
 
   PORT_LED = 0xFF;
 
   sei();
 	for (;;)
   {
-    if (!(PIN_SW & 0x01))
+    PORT_FND = num[cnt];
+    _delay_ms(200);
+    /*if (!(PIN_SW & 0x01))
 		{
       PORT_LED = 0xAF;
-      PORT_FND = num[cnt];
       _delay_ms(200);
       PORT_LED = 0xFF;
-		}
+		}*/
 	}
 }
